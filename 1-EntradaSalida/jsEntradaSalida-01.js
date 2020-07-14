53af408a80a20da26ab92f39d3e242ef9f794653
Andres Torres <torrescaicedoandresfelipe@gmail.com>
2020-07-13 22:19:16 -0300

    TRABAJO 13/07/2020

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,7 +1,10 @@
 /*
 Al presionar el  botón, 
-se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
+se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"
+todo lo que este entre asterisco y barra es un comentario*/
 function mostrar()
 {
-	alert("Esto funciona de maravilla!!");
+	//alert("Esto funciona de maravilla!!!");//esto es un comentario utilizando doble barra.
+	confirm ("DESEAS CONTINUAR")
+	//prompt ("CUAL ES TU NOMBRE")
 }
--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,18 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	//var nombre;/* Notacion camel case? todas las variables tiene que estar en minuscula cuando son de una 
+   // palabra, excepto que sea una palabra compuesta, es decir mas de una palabra*/
+	//var nombreCompleto;	
+	//nombre = "Andres"; /* EL SIGNO IGUAL FUNCIONA COMO OPERADOR DE ASIGNACION SIEMPRE TIENE QUE ESTAR A 
+	//LA IZQUIERDA LA VARIABLE , STRING SIEMPRE TIENE QUE ESTAR EN COMILLAS*/
+	//nombre = 1; //NUMBER
+	//nombre = true; // boolean  Puede ser falso o verdadero
+	//nombre = null // NULL 
+    var dato ;
+	dato = prompt("Ingrese un dato");
+	alert(dato)
+
+	
 }
 
