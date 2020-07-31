46ab58a75bf75808cc1999883ccea46d2a6ad30b
Andres Torres <torrescaicedoandresfelipe@gmail.com>
2020-07-31 09:44:37 -0300

    ARREGLO EJERCICIO 8

--- 5-InstruccionWhile/jsIteracionesWhile-08.js
+++ 5-InstruccionWhile/jsIteracionesWhile-08.js
@@ -3,8 +3,7 @@ Al presionar el botón pedir  números  hasta que el usuario quiera,
 sumar los que son positivos y multiplicar los negativos.*/
 function mostrar()
 {
-	var numeroIngresadoPositivo;
-	var numeroIngresadoNegativo;
+	var numeroIngresado;
 	var contador;
 	var respuesta;
 	var sumaPositivos;
@@ -14,31 +13,30 @@ function mostrar()
 	sumaPositivos=0;
 	multiplicacionNegativos=1;
 	respuesta=true;
-
-	while(respuesta==true)
-	{
-		numeroIngresadoPositivo = prompt("¡Ingrese un numero!");
-		numeroIngresadoPositivo = parseInt(numeroIngresadoPositivo);
-
-		sumaPositivos = sumaPositivos + numeroIngresadoPositivo;
-		contador = contador + 1;
-
-		respuesta = confirm("¿Quiere ingresar mas numeros?");
-		
-	}
-
-	while(respuesta==true)
-	{
-		numeroIngresadoNegativo = prompt("¡Ingrese un numero!");
-		numeroIngresadoNegativo = parseInt(numeroIngresadoNegativo);
-
-			multiplicacionNegativos = (multiplicacionNegativos) * (numeroIngresadoNegativo);
-			contador = contador + 1;
-
+	
+	
+		while(respuesta==true)
+		{
+			numeroIngresado = prompt("¡Ingrese un numero!");
+			numeroIngresado = parseInt(numeroIngresado);
 			respuesta = confirm("¿Quiere ingresar mas numeros?");
-	}
-
-	txtIdSuma.value=sumaPositivos;
-	txtIdProducto.value=multiplicacionNegativos;
+			if(numeroIngresado>0)
+			{
+				sumaPositivos = sumaPositivos + numeroIngresado;
+				contador = contador + 1;
+			}
+			else
+			{
+				//numeroIngresado = prompt("¡Ingrese un numero!");
+				//numeroIngresado = parseInt(numeroIngresado);		
+					multiplicacionNegativos = (multiplicacionNegativos) * (numeroIngresado);
+					contador = contador + 1;
+		
+					//respuesta = confirm("¿Quiere ingresar mas numeros?");				
+			}
+		}
+		txtIdSuma.value = sumaPositivos;
+		txtIdProducto.value = multiplicacionNegativos;
+	
 
 }//FIN DE LA FUNCIÓN
